24aa3448029d2999200ddb1b9808fc7e795bd9e2
Ivanov Anton <3ton.ua@gmail.com>
2016-12-13 11:18:23 +0200

    add

---
+++ app.js
@@ -0,0 +1,71 @@
+var app = angular.module('weather', ['ui.bootstrap']);
+
+app.controller('WeatherController', function ($scope, API) {
+
+
+
+
+    $scope.searchCity = function () {
+        API.SearchCity($scope.searchSetting).then(function (searchs) {
+            $scope.search = searchs;
+            console.log(searchs);
+        });
+
+        API.getForecastWeather().then(function (forecasts) {
+            $scope.forecasts = forecasts;
+            console.log(forecasts);
+        });
+    };
+
+    $scope.searchSetting = {
+        query: 'kiev'
+    };
+
+    $scope.current = [];
+    $scope.forecasts = [];
+    $scope.searchs = [];
+
+});
+
+app.service('API', function ($http, $q) {
+    return{
+        getForecastWeather: function (params) {
+            var key = 'ad5d39cc015543028f6203619161811';
+            var d = $q.defer();
+            $http({
+                method: 'GET',
+                url: 'http://api.apixu.com/v1/forecast.json',
+                params: {
+                    part: "snippet",
+                    key: key,
+                    q: params.query,
+                    days: 5
+                }
+            }).then(function (data) {
+                console.log(data.data.forecast);
+                console.log(params.query);
+                d.resolve(data)
+            });
+            return d.promise
+        },
+        SearchCity: function (params) {
+            var key = 'ad5d39cc015543028f6203619161811';
+            var d = $q.defer();
+            $http({
+                method: 'GET',
+                url: 'http://api.apixu.com/v1/search.json',
+                params: {
+                    key : key,
+                    q: params.query
+                }
+            }).then(function (data) {
+                var search = data.data[0];
+                d.resolve(search);
+                console.log(search);
+                console.log(params.query)
+            });
+            return d.promise;
+        }
+    }
+});
+
